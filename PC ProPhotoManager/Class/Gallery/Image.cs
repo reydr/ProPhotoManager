c4f92e439883029a86540cc381b966512a15cbb8
Redor <37882122+reydr@users.noreply.github.com>
2019-04-21 14:23:08 +0300

    -

---
+++ PC ProPhotoManager/Class/Gallery/Image.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PC_ProPhotoManager.Class.Gallery
+{
+    public sealed class Image : Gallery
+    {
+
+    }
+}
